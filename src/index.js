b002b268c4315359b383277864cb26390f63738c
HARRISKING <llc13588440410@outlook.com>
2017-06-15 15:31:26 +0800

    6.15

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import Welcome from './Welcome'
+
+ReactDOM.render(
+  <Welcome/>,
+  document.getElementById('root')
+)
 
 // function tick(){
 //   const element = (
--- src/welcome.js
+++ src/welcome.js
@@ -1,4 +1,7 @@
 import React from 'react'; // 为什么要 import React
+//因为新建的welcome需要作为react.component的子类，需要引用react。
+
+
 class Welcome extends React.Component {
   render() {
     return <h1>Hello, Component</h1>;
@@ -6,3 +9,5 @@ class Welcome extends React.Component {
 }
 
 export default Welcome // 为什么要 export，为什么要加 default
+//export是为了暴露接口给其他调用welcome的文件；
+//export default 是默认导出，只想到出一个简单值或者想在模块中保留一个候选值时采用
